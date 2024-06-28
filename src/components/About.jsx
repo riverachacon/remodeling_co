a5b705debd4b632cf4cf7dc45e77f57365fed4d5
Giovanni Rivera Chacon <princecito@Giovannis-Air.lan>
2024-06-28 12:22:44 -0400

    update About code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,3 @@
-// function About() {
-//   const customStyle = {
-//     p1: {
-//       fontSize: "1.4rem",
-//       color: "#606C38",
-//     },
-//   };
-//   return (
-//     <div id="about" className="aboutUs">
-//       <h1 style={{ fontSize: "3.5rem" }}>About Us</h1>
-//       <div className="aboutText">
-//         <p style={customStyle.p1}>
-//           {" "}
-//           With more than 76 percent of our business is either repeat customers
-//           or referral based business; surely we are doing something right!
-//         </p>
-//         <p>
-//           With over 8 years of experience, we specialize in making your home
-//           look its best. Our team focuses on remodeling and restoring homes,
-//           including bathrooms, kitchens, living rooms, and outdoor spaces. We
-//           are dedicated to excellent work and meticulous in every detail. Choose
-//           Renoble for your next project and experience the difference our work
-//           makes.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default About;
-
 import React, { useEffect, useState } from "react";
 
 function About() {
